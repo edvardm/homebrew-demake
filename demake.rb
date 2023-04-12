70c5b28219e852818d6a6ddfd1dd3639306f6a0b
Edvard Majakari <edvard@majakari.net>
2023-04-12 21:00:19 +0300

    release 0.1.4.2

--- demake.rb
+++ demake.rb
@@ -5,8 +5,8 @@
 class Demake < Formula
   desc "Helper to translate Makefile rules to PyInvoke tasks.py file"
   homepage "https://github.com/edvardm/demake"
-  url "https://github.com/edvardm/demake/archive/refs/tags/0.1.2.tar.gz"
-  sha256 "9af6eb54639e0270534b895f67dd9b73caab371c6862e37a82d8011a2e6f1731"
+  url "https://github.com/edvardm/demake/archive/refs/tags/0.1.4.2.tar.gz"
+  sha256 "f844d9a4a87f85e623bc683a7a98e0a09a4f4b3b2c9c8afde8ebb41aaec75bd5"
   license "MIT"
 
   depends_on "cabal-install" => :build
